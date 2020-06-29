384f22c90fcf5419a3ee557e23f671beba75fc24
franTr <mrt20694@gmail.com>
2020-06-29 21:52:44 +0200

    solution exercise 1

--- exercise/problem/requests.go
+++ exercise/problem/requests.go
@@ -4,23 +4,29 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"sync"
+	"log"
 )
 
 func main() {
+	var wg sync.WaitGroup
 	sites := []string{
 		"https://www.google.com",
 		"https://drive.google.com",
 		"https://maps.google.com",
-		"https://hangouts.google.com",
+		"https://hangouts.goggle.com",
 	}
-
 	for _, site := range sites {
-		go func(site string) {
+		wg.Add(1)
+		go func(site string, wg *sync.WaitGroup) {
+			defer wg.Done()
 			res, err := http.Get(site)
 			if err != nil {
+				log.Fatal(err)
 			}
-
 			io.WriteString(os.Stdout, res.Status+"\n")
-		}(site)
+
+		}(site, &wg)
 	}
+	wg.Wait()
 }
