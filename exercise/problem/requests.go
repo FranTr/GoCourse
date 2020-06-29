451527c610b200f7e54cf08232d6fcd53f2dffd6
franTr <mrt20694@gmail.com>
2020-06-29 21:57:32 +0200

    removed not necessary parameter in goroutine

--- exercise/problem/requests.go
+++ exercise/problem/requests.go
@@ -18,15 +18,15 @@ func main() {
 	}
 	for _, site := range sites {
 		wg.Add(1)
-		go func(site string, wg *sync.WaitGroup) {
-			defer wg.Done()
+		go func(site string) {
+            defer wg.Done()
 			res, err := http.Get(site)
 			if err != nil {
 				log.Fatal(err)
 			}
 			io.WriteString(os.Stdout, res.Status+"\n")
 
-		}(site, &wg)
+		}(site)
 	}
 	wg.Wait()
 }
